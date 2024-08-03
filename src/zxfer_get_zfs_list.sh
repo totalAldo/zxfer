b1b1f7745e15c788062e9fb10f9894f327f8a182
Aldo Gonzalez <totalAldo@gmail.com>
2024-08-03 17:15:25 -0400

    clean up the debug output of diffs

--- src/zxfer_get_zfs_list.sh
+++ src/zxfer_get_zfs_list.sh
@@ -181,8 +181,6 @@ set_g_recursive_source_list() {
         echo "g_recursive_source_list:"
         echo "$g_recursive_source_list"
         echo "Source dataset count: $(echo "$g_recursive_source_list" | grep -cve '^\s*$')"
-        echo "====== Destination datasets missing snapshots from source ======"
-        echo "$g_recursive_source_list" | "$g_cmd_awk" -F@ '{print $1}' | sort -u
         echo "====================================================================="
         echo "====== Extra Destination snapshots not in source ======"
         comm -13 "$l_source_snaps_sorted_tmp_file" "$l_dest_snaps_stripped_sorted_tmp_file"
