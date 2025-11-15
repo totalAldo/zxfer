76d775cae855108dccf24836e328a1981be1b388
Aldo Gonzalez <totalAldo@gmail.com>
2025-11-14 22:24:16 -0500

    version bump

--- src/zxfer_globals.sh
+++ src/zxfer_globals.sh
@@ -42,7 +42,7 @@
 #
 init_globals() {
     # zxfer version
-    g_zxfer_version="2.0.0-20240919"
+    g_zxfer_version="2.0.1-20251114"
 
     # max number of iterations to run iterate through run_zfs_mode
     # if changes are made to the filesystems
