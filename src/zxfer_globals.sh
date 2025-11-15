cd598511012d4a018b24159750ec67081cabd030
Aldo Gonzalez <totalAldo@gmail.com>
2025-11-15 08:55:23 -0500

    add .shellcheckrc

--- src/zxfer_globals.sh
+++ src/zxfer_globals.sh
@@ -42,7 +42,7 @@
 #
 init_globals() {
     # zxfer version
-    g_zxfer_version="2.0.1-20251114"
+    g_zxfer_version="2.0.1-20251115"
 
     # max number of iterations to run iterate through run_zfs_mode
     # if changes are made to the filesystems
