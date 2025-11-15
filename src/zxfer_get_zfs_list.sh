2f18c5400a81ea808717cbf1d8d05f7dc6de18f4
Aldo Gonzalez <totalAldo@gmail.com>
2025-11-15 13:09:24 -0500

    Fix GNU parallel snapshot listing
    Ensure the commands handed to GNU parallel expand $g_cmd_zfs/$g_LZFS before the worker shells run by escaping them into double quotes, rather than single-quoting the template. This restores multi-job snapshot discovery because the worker processes now execute the actual zfs binary instead of looking for a command literally named list.

--- src/zxfer_get_zfs_list.sh
+++ src/zxfer_get_zfs_list.sh
@@ -72,15 +72,18 @@ write_source_snapshot_list_to_file() {
 			# zstd -19 takes too long
 
 			# check if compression is enabled
+			l_remote_parallel_cmd=$(escape_for_double_quotes "$g_cmd_zfs list -H -o name -s creation -d 1 -t snapshot {}")
+
 			if [ "$g_option_z_compress" -eq 1 ]; then
 				# IllumOS requires -d 1 when listing snapshots for one dataset
-				l_cmd="$l_origin_ssh_cmd $g_option_O_origin_host \"$g_cmd_zfs list -Hr -o name $initial_source | $g_cmd_parallel -j $g_option_j_jobs --line-buffer '$g_cmd_zfs list -H -o name -s creation -d 1 -t snapshot {}' | zstd -9\" | zstd -d"
+				l_cmd="$l_origin_ssh_cmd $g_option_O_origin_host \"$g_cmd_zfs list -Hr -o name $initial_source | $g_cmd_parallel -j $g_option_j_jobs --line-buffer \\\"$l_remote_parallel_cmd\\\" | zstd -9\" | zstd -d"
 			else
-				l_cmd="$l_origin_ssh_cmd $g_option_O_origin_host \"$g_cmd_zfs list -Hr -o name $initial_source | $g_cmd_parallel -j $g_option_j_jobs --line-buffer '$g_cmd_zfs list -H -o name -s creation -d 1 -t snapshot {}'\""
+				l_cmd="$l_origin_ssh_cmd $g_option_O_origin_host \"$g_cmd_zfs list -Hr -o name $initial_source | $g_cmd_parallel -j $g_option_j_jobs --line-buffer \\\"$l_remote_parallel_cmd\\\"\""
 			fi
 		else
 			#l_cmd="$g_LZFS list -Hr -o name $initial_source | xargs -n 1 -P $g_option_j_jobs -I {} sh -c '$g_LZFS list -H -o name -s creation -d 1 -t snapshot {}'"
-			l_cmd="$g_LZFS list -Hr -o name $initial_source | $g_cmd_parallel -j $g_option_j_jobs --line-buffer '$g_LZFS list -H -o name -s creation -d 1 -t snapshot {}'"
+			l_local_parallel_cmd=$(escape_for_double_quotes "$g_LZFS list -H -o name -s creation -d 1 -t snapshot {}")
+			l_cmd="$g_LZFS list -Hr -o name $initial_source | $g_cmd_parallel -j $g_option_j_jobs --line-buffer \\\"$l_local_parallel_cmd\\\""
 		fi
 
 		echoV "Running command in the background: $l_cmd"
